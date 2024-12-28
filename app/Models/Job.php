bf852428e66bdf9b26ab59a43c23cc5e9a51d0fe
Z.D. <zd.1.6.abb@gmail.com>
2024-12-28 22:57:08 +0200

    Day 16 - forms

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,16 +10,10 @@ class Job extends Model {
 
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $guarded = [];
 
     public function employer()
     {
         return $this->belongsTo(Employer::class);
-    }
-
-    public function tags()
-    {
-        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
-
     }
 }
