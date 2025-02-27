04a404f3ee72178b18e84d04866998119e659d16
Z.D. <zd.1.6.abb@gmail.com>
2025-02-27 12:29:32 +0200

    modify register to use blade components

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,70 +1,17 @@
 <x-layout>
-    <x-slot:heading>
-        Register
-    </x-slot:heading>
+    <x-page-heading>Регистрация</x-page-heading>
 
-    <form method="POST" action="/register">
-        @csrf
+    <x-forms.form method="POST" action="/public/register" enctype="multipart/form-data">
+        <x-forms.input label="Име" name="name" />
+        <x-forms.input label="Email" name="email" type="email" />
+        <x-forms.input label="Парола" name="password" type="password" />
+        <x-forms.input label="Парола - потвърждение" name="password_confirmation" type="password" />
 
-        <div class="space-y-12">
-            <div class="border-b border-gray-900/10 pb-12">
-                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <x-form-field>
-                        <x-form-label for="first_name">First Name</x-form-label>
+        <x-forms.divider />
 
-                        <div class="mt-2">
-                            <x-form-input name="first_name" id="first_name" required />
+        <x-forms.input label="Работодател" name="employer" />
+        <x-forms.input label="Лого на работодателя" name="logo" type="file" />
 
-                            <x-form-error name="first_name" />
-                        </div>
-                    </x-form-field>
-
-                    <x-form-field>
-                        <x-form-label for="last_name">Last Name</x-form-label>
-
-                        <div class="mt-2">
-                            <x-form-input name="last_name" id="last_name" required />
-
-                            <x-form-error name="last_name" />
-                        </div>
-                    </x-form-field>
-
-                    <x-form-field>
-                        <x-form-label for="email">Email</x-form-label>
-
-                        <div class="mt-2">
-                            <x-form-input name="email" id="email" type="email" required />
-
-                            <x-form-error name="email" />
-                        </div>
-                    </x-form-field>
-
-                    <x-form-field>
-                        <x-form-label for="password">Password</x-form-label>
-
-                        <div class="mt-2">
-                            <x-form-input name="password" id="password" type="password" required />
-
-                            <x-form-error name="password" />
-                        </div>
-                    </x-form-field>
-
-                    <x-form-field>
-                        <x-form-label for="password_confirmation">Confirm Password</x-form-label>
-
-                        <div class="mt-2">
-                            <x-form-input name="password_confirmation" id="password_confirmation" type="password" required />
-
-                            <x-form-error name="password_confirmation" />
-                        </div>
-                    </x-form-field>
-                </div>
-            </div>
-        </div>
-
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-            <x-form-button>Register</x-form-button>
-        </div>
-    </form>
+        <x-forms.button>Създай потребител</x-forms.button>
+    </x-forms.form>
 </x-layout>
