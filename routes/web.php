dea65972ded0a1139686d032378d5371011cf0b7
Z.D. <zd.1.6.abb@gmail.com>
2025-02-27 06:39:00 +0200

    changed routes to reflect most recent

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,43 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\SearchReservationController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\VatinvoiceController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-Route::view('/contact', 'contact');
+use App\Http\Livewire\ReservationSearch;
 
- Route::get('/jobs', [JobController::class, 'index']);
- Route::get('/jobs/create', [JobController::class, 'create']);
- Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
- Route::get('/jobs/{job}', [JobController::class, 'show']);
 
- Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
-     ->middleware('auth')
-     ->can('edit', 'job');
+Route::get('/', [JobController::class, 'index']);
 
- Route::patch('/jobs/{job}', [JobController::class, 'update']);
- Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
 
-// Auth
-Route::get('/register', [RegisteredUserController::class, 'create']);
-Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::get('/airbnb_reservations/reservations', [ReservationController::class, 'reservations'])->middleware('auth');
 
-Route::get('/login', [SessionController::class, 'create'])->name('login');
-Route::post('/login', [SessionController::class, 'store']);
-Route::post('/logout', [SessionController::class, 'destroy']);
+Route::get('/airbnb_reservations/vatinvoices', [VatinvoiceController::class, 'vatinvoices'])->middleware('auth');
 
+Route::get('/public/search_reservations', SearchReservationController::class);
+
+Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
+
+});
+
+Route::delete('/logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::get('/public/airbnb_reservations/reservations', [ReservationController::class, 'reservations'])->name('reservations.index');
+
+Route::get('/public/airbnb_reservations/search', [ReservationController::class, 'search'])->name('reservations.search');
